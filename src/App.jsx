c68c4094e7312464a75ff28202ff5d5040e5a273
Caio Barbosa <caiomontebarbosa104@gmail.com>
2025-08-05 17:58:19 -0300

    comeco produtos

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,22 @@ function App() {
         <>
             <Navbar onMenuClick={() => setSidebarOpen(true)} />
             <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+            <main className="p-6 h-full w-full flex flex-col items-center justify-betweenfont-mono">
+                <h1 className="text-3xl font-bold">Produtos</h1>
+                <section className="mt-4 w-full h-80 max-w-6xl bg-black shadow-md rounded-lg p-6 flex justify-around items-center gap-4">
+                    <div className="flex flex-col items-center justify-center h-10/12 w-4xl bg-white rounded-lg shadow-lg">
+                        .
+                    </div>
+                    <div className="flex flex-col items-center justify-center h-10/12 w-4xl bg-white rounded-lg shadow-lg">
+                        .
+                    </div>
+                    <div className="flex flex-col items-center justify-center h-10/12 w-4xl bg-white rounded-lg shadow-lg">
+                        .
+                    </div>
+
+                </section>
+            </main>
+
         </>
     );
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -16,13 +16,13 @@ function Sidebar({ open, onClose }) {
                     <h1 className="text-2xl font-bold text-center font-mono">MENU</h1>
                     <i className="bx  bx-x  cursor-pointer" onClick={onClose}></i>
                 </div>
-                <ul className="flex w-full text-center flex-col justify-center items-center h-10/12 gap-20 ">
+                <ul className="flex w-full bg-white text-center flex-col justify-center items-center h-10/12 gap-20 ">
                     <ListaSidebar nome="Home" />
                     <ListaSidebar nome="Sobre Nós" />
                     <ListaSidebar nome="Informações" />
                 </ul>
-                <div className="flex justify-center items-center w-full h-13 gap-1 bg-sky-900 text-white">
-                    <div className="flex items-center justify-center p-2 rounded-3xl w-1/2 hover:text-black hover:bg-yellow-400 transition-colors duration-300 cursor-pointer gap-1">
+                <div className="flex justify-center items-center w-full h-13 gap-1 bg-sky-800 text-white">
+                    <div className="flex items-center justify-center p-2 rounded-3xl w-1/2 hover:text-black hover:bg-amber-400 transition-colors duration-300 cursor-pointer gap-1">
                         <p>Telefones</p>
                         <i className="bx bx-phone"></i>
                     </div>
