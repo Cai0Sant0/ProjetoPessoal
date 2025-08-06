763da36c262835e934a24737938c4cc301c9592d
Caio Barbosa <caiomontebarbosa104@gmail.com>
2025-08-06 13:10:13 -0300

    trabalhando na página principal

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,30 @@ function App() {
         <>
             <Navbar onMenuClick={() => setSidebarOpen(true)} />
             <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-            <main className="p-6 h-full w-full flex flex-col items-center justify-betweenfont-mono">
-                <h1 className="text-3xl font-bold">Produtos</h1>
-                <section className="mt-4 w-full h-80 max-w-6xl bg-black shadow-md rounded-lg p-6 flex justify-around items-center gap-4">
-                    <div className="flex flex-col items-center justify-center h-10/12 w-4xl bg-white rounded-lg shadow-lg">
-                        .
-                    </div>
-                    <div className="flex flex-col items-center justify-center h-10/12 w-4xl bg-white rounded-lg shadow-lg">
-                        .
-                    </div>
-                    <div className="flex flex-col items-center justify-center h-10/12 w-4xl bg-white rounded-lg shadow-lg">
-                        .
-                    </div>
-
-                </section>
+            <h1 className="w-full text-2xl text-center font-bold font-mono p-6 md:text-4xl">Produtos Em Destaque!</h1>
+            <main className="grid grid-cols-1 h-200 bg-amber-500 p-2 gap-2 md:grid-cols-4 md:h-100 md:p-4">
+                <div className="p-4 bg-white shadow-md rounded-lg  flex flex-col justify-around items-center">
+                    <h2 className="text-xl font-semibold">Produto 1</h2>
+                    <p className="text-gray-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste magni cumque vero ipsum maiores! Magnam iure doloribus,</p>
+                </div>
+                  <div className="p-4 bg-white shadow-md rounded-lg  flex flex-col justify-around items-center">
+                    <h2 className="text-xl font-semibold">Produto 1</h2>
+                    <p className="text-gray-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste magni cumque vero ipsum maiores! Magnam iure doloribus,</p>
+                </div>
+                  <div className="p-4 bg-white shadow-md rounded-lg  flex flex-col justify-around items-center">
+                    <h2 className="text-xl font-semibold">Produto 1</h2>
+                    <p className="text-gray-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste magni cumque vero ipsum maiores! Magnam iure doloribus,</p>
+                </div>
+                <div className="p-4 bg-white shadow-md rounded-lg  flex flex-col justify-around items-center">
+                    <h2 className="text-xl font-semibold">Produto 1</h2>
+                    <p className="text-gray-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste magni cumque vero ipsum maiores! Magnam iure doloribus,</p>
+                </div>
             </main>
-
+            <footer className="mt-4 bg-amber-400">
+                <div className="bg-gray-800 text-white text-center p-4">
+                    <p>&copy; 2023 Meu Site. Todos os direitos reservados.</p>
+                </div>
+            </footer>
         </>
     );
 }
