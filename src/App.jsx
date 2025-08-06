14d5a2ed38d2318b6afb59836b406003dd90db3b
Caio Barbosa <caiomontebarbosa104@gmail.com>
2025-08-06 17:57:41 -0300

    mudança nos cards para por imagem

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import CardItem from "./components/CardItem/CardItem";
 import Rodape from "./components/Rodape/Rodape";
+
 import { useState } from "react";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
             <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
             <h1 className="w-full text-2xl text-center font-bold font-mono p-6 md:text-4xl">Produtos Em Destaque!</h1>
             <main className="grid grid-cols-1 md:grid-cols-4 min-h-svh p-8 gap-4">
-                <CardItem nomeProd="Palmilha" descProd="Palmilha anatômica em couro legítimo, conforto e durabilidade para seus pés." />
+                <CardItem nomeProd="Palmilha" descProd="Palmilha anatômica em couro legítimo, conforto e durabilidade para seus pés."/>
                 <CardItem nomeProd="Carteira " descProd="Carteira masculina em couro premium, design clássico e elegante." />
                 <CardItem nomeProd="Bolsa " descProd="Bolsa feminina em couro natural, perfeita para todas as ocasiões." />
                 <CardItem nomeProd="Cinto" 
