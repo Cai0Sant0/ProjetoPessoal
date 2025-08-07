b89cf9fa15b683100cc0f918b974afb0ac1c8f44
Caio Barbosa <caiomontebarbosa104@gmail.com>
2025-08-07 09:47:58 -0300

    cards finalizados

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,12 +1,15 @@
 function CardItem(props) {
     return (
         <div className=" bg-white rounded-lg flex flex-col justify-baseline items-center gap-3 h-96 border-2 border-gray-300 shadow-lg hover:border-gray-500 hover:shadow-xl transition-all duration-300">
-            <div className={`w-full h-1/2 rounded-t-lg border-gray-300 shadow-lg bg-[url(${props.imgUrl})] bg-cover bg-center`}></div>
+            <div
+                className="w-full h-1/2 rounded-t-lg border-gray-300 shadow-lg bg-contain bg-center bg-no-repeat"
+                style={{ backgroundImage: `url(${props.imgUrl})` }}
+            ></div>
             <div className="flex flex-col">
                     <h2 className="text-center">{props.nomeProd}</h2>
                 <p className="text-gray-700 p-4 text-sm md:text-sm text-center">{props.descProd}</p>
             </div>
-            <a href="">Acessar o Catálogo<i className='bx  bx-book ml-0.5 '  ></i></a>
+            <a href="" className="border-gray-400 backdrop-blur-2xl bg-black/40 text-white border-2 p-2 rounded-4xl hover:border-2 hover:border-sky-700 transition-all hover:bg-white hover:text-black">Acessar o Catálogo<i className='bx  bx-book ml-1 '  ></i></a>
         </div>
     );
 }
